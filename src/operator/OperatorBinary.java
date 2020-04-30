bea2d0402aa19e93675a8fa3046f2c8effcc2df7
adellarezky <adellarezky99@gmail.com>
2020-04-30 23:46:09 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai ke dalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Adella Rezky Dwiananda";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+
+}
